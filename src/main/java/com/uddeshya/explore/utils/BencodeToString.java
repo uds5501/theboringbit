0a787491647a3e2a57c7748a59ee900b187dc74f
Uddeshya Singh <uddeshya.singh@gojek.com>
2022-09-17 00:44:01 +0530

    Finish string parsing for List and Dictionary

--- src/main/java/com/uddeshya/explore/utils/BencodeToString.java
+++ src/main/java/com/uddeshya/explore/utils/BencodeToString.java
@@ -16,7 +16,13 @@ public class BencodeToString {
         return result;
     }
 
-    public static String convertFromMap(Map<String, BencodeObject> valueAsDictionary) {
-        return "";
+    public static String convertFromMap(Map<String, BencodeObject> givenDict) {
+        String result = "DICT{";
+        for(Map.Entry<String,BencodeObject> entry: givenDict.entrySet()) {
+            result += entry.getKey() + ":" + entry.getValue().toString() + ",";
+        }
+        result = result.substring(0, result.length()-1);
+        result += "}";
+        return result;
     }
 }
