a4c85bb6f4bdf15be71ae402dcd7376c7926acd2
Uddeshya Singh <uddeshya.singh@gojek.com>
2022-09-17 22:37:47 +0530

    Finalize Bencrpyter decrypt module
    - Test the decryption module with success cases
    - Add PMD tests
    - Add Tests for BencodeToNative conversions for List and Dicts

--- src/main/java/com/uddeshya/explore/utils/BencodeToString.java
+++ src/main/java/com/uddeshya/explore/utils/BencodeToString.java
@@ -8,20 +8,20 @@ import java.util.Map;
 public class BencodeToString {
     public static String convertFromList(List<BencodeObject> givenList) {
         String result = "LIST[";
-        for(int i=0; i<givenList.size(); i++) {
-            result = result + givenList.get(i).toString() + ",";
+        for (BencodeObject bo : givenList) {
+            result += bo.toString() + ",";
         }
-        result = result.substring(0, result.length()-1);
+        result = result.substring(0, result.length() - 1);
         result += "]";
         return result;
     }
 
     public static String convertFromMap(Map<String, BencodeObject> givenDict) {
         String result = "DICT{";
-        for(Map.Entry<String,BencodeObject> entry: givenDict.entrySet()) {
+        for (Map.Entry<String, BencodeObject> entry : givenDict.entrySet()) {
             result += entry.getKey() + ":" + entry.getValue().toString() + ",";
         }
-        result = result.substring(0, result.length()-1);
+        result = result.substring(0, result.length() - 1);
         result += "}";
         return result;
     }
