8b27df59565251c2d9eb894d0f5e0859be9fdf31
Uddeshya Singh <uddeshya.singh@gojek.com>
2022-09-17 23:06:33 +0530

    Complete continous string parsing mechanism individually

---
+++ src/main/java/com/uddeshya/explore/model/StringParsingResult.java
@@ -0,0 +1,38 @@
+package com.uddeshya.explore.model;
+
+public class StringParsingResult {
+    private String data;
+    private int stringLength;
+    private boolean success;
+
+    public StringParsingResult(String data, boolean success) {
+        this.data = data;
+        this.success = success;
+        this.stringLength = 0;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getStringLength() {
+        return stringLength;
+    }
+
+    public void setStringLength(int stringLength) {
+        this.stringLength = stringLength;
+    }
+}
+
