acd1304319585138ca20bb61a6e74aa46dc1cb34
Uddeshya Singh <uddeshya.singh@gojek.com>
2022-09-17 23:14:35 +0530

    Complete continous integer parsing mechanism individually

--- src/main/java/com/uddeshya/explore/utils/StackParser.java
+++ src/main/java/com/uddeshya/explore/utils/StackParser.java
@@ -1,5 +1,6 @@
 package com.uddeshya.explore.utils;
 
+import com.uddeshya.explore.model.IntegerParsingResult;
 import com.uddeshya.explore.model.StringParsingResult;
 
 public class StackParser {
@@ -24,6 +25,22 @@ public class StackParser {
         }
         result.setData(str);
         result.setSuccess(true);
+        return result;
+    }
+
+    public static IntegerParsingResult parseIntegerFromIndex(String original, int i) {
+        IntegerParsingResult result = new IntegerParsingResult(false);
+        i++;
+        int intData = 0;
+        while (original.charAt(i) != 'e') {
+            if (original.charAt(i) < '0' || original.charAt(i) > '9') {
+                return result;
+            }
+            intData = intData * 10 + (original.charAt(i) - '0');
+            i++;
+        }
+        result.setSuccess(true);
+        result.setData(intData);
         return result;
     }
 }
